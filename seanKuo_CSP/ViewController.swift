d3639a8e93be315f9a89c62a43e47c27ac3b5220
kuojsean <kuo.j.sean@gmail.com>
2017-10-26 07:56:41 -0600

    Created CSP Project and changed ignore settings.

---
+++ seanKuo_CSP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  seanKuo_CSP
+//
+//  Created by Kuo, Sean on 10/26/17.
+//  Copyright © 2017 Kuo, Sean. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
