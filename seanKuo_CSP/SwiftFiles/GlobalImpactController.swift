931ad4012fa7ff92b828d8a342a24b0552386578
kuojsean <kuo.j.sean@gmail.com>
2017-11-14 07:49:19 -0700

    created the second label programatically.

--- seanKuo_CSP/SwiftFiles/GlobalImpactController.swift
+++ seanKuo_CSP/SwiftFiles/GlobalImpactController.swift
@@ -17,7 +17,7 @@ class GlobalImpactController: UIViewController {
     {
         groupSketch.image = UIImage(named: "groupSketch")
         self.addressProblem.text = "This app would address the problem of water. The water problem is huge with over 783 million not having access to clean water and over 2.1 billion lacking a sanitary facility to make clean water. Because of this, there are several people affected by disease and death around the world due to this water problem."
-        
+        self.personalConnection.text = "I personally am not affected by the clean water program but my family members live overseas in Taiwan. Although Taiwan provides a very great sanitary area in the metro region, places out of Taiwan where more distant relatives don't live in such great conditions. Because of this, they have a high chance that the water they use and consume may not be completely sanitary."
     }
     
     override func viewDidLoad() {
