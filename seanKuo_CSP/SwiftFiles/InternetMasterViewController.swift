2d8af40b0b0a87484f88612883b529b662157ca1
kuojsean <kuo.j.sean@gmail.com>
2017-12-01 09:38:09 -0700

    Created the detail view controller and updated the master view controller code.

--- seanKuo_CSP/SwiftFiles/InternetMasterViewController.swift
+++ seanKuo_CSP/SwiftFiles/InternetMasterViewController.swift
@@ -22,11 +22,27 @@ class InternetMasterViewController: UITableViewController
             ]
     }()
     
+    private lazy var addresses : [String] = []
+    
     private var detailViewController : InternetDetailViewController?
     
     private func setup() -> Void
     {
+        //TODO: Replace with your correct links
+        addresses = [
+            "https://www.google.com",
+            "https://apstudent.collegeboard.org/apcourse/ap-computer-science-principles",
+            "http://www.canyonsdistrict.org/",
+            "https://ctec.canyonsdistrict.org/",
+            "https://developer.apple.com/library/content/documentation/Swift/Conceptual/Swift_Programming_Language/TheBasics.html#//apple_ref/doc/uid/TP40014097-CH5-ID309",
+            "https://twitter.com/"
+        ]
         
+        if let splitView = splitViewController
+        {
+            let currentControllers = splitView.viewControllers
+            detailViewController = currentControllers[0] as? InternetDetailViewController
+        }
     }
     
     override func viewDidLoad()
@@ -51,7 +67,7 @@ class InternetMasterViewController: UITableViewController
         return internetTopics.count
     }
     
-    override public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableView
+    override public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
     {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
         let currentText = internetTopics[indexPath.row]
@@ -60,4 +76,33 @@ class InternetMasterViewController: UITableViewController
         return cell
     }
     
+    //MARK: Handle the internal transfer
+    override public func prepare(for segue: UIStoryboardSegue, sender: Any?)
+    {
+        if segue.identifier! == "showDetail"
+        {
+            if let indexPath = self.tableView.indexPathforSelectedRow
+            {
+                let urlString = addresses[indexPath.row]
+                let pageText : String
+                
+                if indexPath.row == 0
+                {
+                    //TODO: Replace with your definitions - great time to use the """ operator
+                    pageText = "All the definitions you wrote......."
+                }
+                else
+                {
+                    pageText = internetTopics[indexPath.row]
+                }
+                
+                let controller = segue.destination as! InternetDetailViewController
+                
+                controller.detailAddress = urlString
+                controller.detailText = pageText
+                controller.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
+                controller.navigationItem.leftItemsSupplementBackButton = true
+            }
+        }
+    }
 }
