d0c3f26111dfa97154b8d95cbc06669574300e51
kuojsean <kuo.j.sean@gmail.com>
2017-12-07 09:22:04 -0700

    Created the collectioview controller on the main storyboard.

---
+++ seanKuo_CSP/View/ArtCell.swift
@@ -0,0 +1,18 @@
+//
+//  ArtCell.swift
+//  seanKuo_CSP
+//
+//  Created by Kuo, Sean on 12/7/17.
+//  Copyright © 2017 Kuo, Sean. All rights reserved.
+//
+
+import UIKit
+
+class ArtCell: UICollectionViewCell
+{
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var imageName: UILabel!
+    
+}
