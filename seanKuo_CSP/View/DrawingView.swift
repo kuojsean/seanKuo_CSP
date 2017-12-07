667681f2aa8e95eccf3e07b2fe7ad00c5fb2850c
kuojsean <kuo.j.sean@gmail.com>
2017-12-07 09:54:28 -0700

    Created the drawTurtle method.

--- seanKuo_CSP/View/DrawingView.swift
+++ seanKuo_CSP/View/DrawingView.swift
@@ -46,7 +46,13 @@ class DrawingView: UIView{
     
     private func drawTurtle() -> Void
     {
-        
+        let logo = UIBezierPath()
+        UIColor.white.setFill()
+        logo.move(to: CGPoint(x: 50, y:250))
+        logo.addLine(to: CGPoint(x: 100, y: 300))
+        logo.addLine(to: CGPoint (x: 50, y: 350))
+        logo.close()
+        logo.fill()
     }
     
     private func drawHappyTree() -> Void
