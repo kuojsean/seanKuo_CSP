0600a91317bcccb1c8eb961ce89383505c3611dc
kuojsean <kuo.j.sean@gmail.com>
2017-12-13 09:38:12 -0700

    Rearranged the files so that it builds without errors.

--- seanKuo_CSP/Controller/ProgrammingViewController.swift
+++ seanKuo_CSP/Controller/ProgrammingViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SpriteKit
 
 class ProgrammingViewController: UIViewController
 {
@@ -22,7 +23,7 @@ class ProgrammingViewController: UIViewController
         gameView.showsNodeCount = true
         gameView.ignoresSiblingOrder = true
         
-        gameScene.scaleMode = resizeFill
+        gameScene.scaleMode = .resizeFill
         gameView.presentScene(gameScene)
     }
 
