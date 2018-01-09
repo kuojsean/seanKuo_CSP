db380a78136b6e5465f07a6cf9e79dc484209f1d
kuojsean <kuo.j.sean@gmail.com>
2018-01-09 08:38:17 -0700

    Created all the content of DataViewController.

--- seanKuo_CSP/Controller/DataView/DataViewController.swift
+++ seanKuo_CSP/Controller/DataView/DataViewController.swift
@@ -10,5 +10,54 @@ import UIKit
 
 class DataViewController: UITableViewController
 {
+    lazy var bucketList : [BucketItem] =
+        {
+            return loadBucketListFromFile()
+        }()
     
+    private func loadBucketListFromFile() -> [BucketItem]
+    {
+        var items = [BucketItem]()
+        if let filePath = Bundle.main.url(forResource : "bucket", withExtension: "csv")
+        {
+            do
+            {
+                let input = try String(contentsOf: filePath)
+                let bucketLines = input.components(separatedBy: "\n")
+                for line in bucketLines
+                {
+                    let item = line.components(separatedBy: ",")
+                    items.append(BucketItem(contents: item[0],author: item [1]))
+                }
+            }
+            catch
+            {
+                print("File load error")
+            }
+        }
+        
+        return items
+    }
+    
+    //MARK: TableView code
+    
+    override public func numberOfSections(in tableView: UITableView) -> Int
+    {
+        return 1
+    }
+    
+    override public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return bucketList.count
+    }
+    
+    override public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let currentCell = tableView.dequeueReusableCell(withIdentifier: "dataIdentifier", for: indexPath) as! BucketItemCell
+        
+        currentCell.bucketItem = bucketList[indexPath.row]
+        //        currentCell.bucketItemSignature.text = cuurentCell.bucketItem.itemAuthor
+        
+        return currentCell
+    }
 }
