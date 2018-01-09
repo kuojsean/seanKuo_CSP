fb27a595b5e42fdd8f6b4993b91d2000741ab675
kuojsean <kuo.j.sean@gmail.com>
2018-01-09 07:46:22 -0700

    Added all files or the Data Page and organized files.

---
+++ seanKuo_CSP/Controller/DataView/BucketItemCell.swift
@@ -0,0 +1,14 @@
+//
+//  BucketItemCell.swift
+//  seanKuo_CSP
+//
+//  Created by Kuo, Sean on 1/9/18.
+//  Copyright © 2018 Kuo, Sean. All rights reserved.
+//
+
+import UIKit
+
+class BucketItemCell: UITableViewCell
+{
+    
+}
---
+++ seanKuo_CSP/Model/DataView/BucketItem.swift
@@ -0,0 +1,9 @@
+//
+//  BucketItem.swift
+//  seanKuo_CSP
+//
+//  Created by Kuo, Sean on 1/9/18.
+//  Copyright © 2018 Kuo, Sean. All rights reserved.
+//
+
+import Foundation
